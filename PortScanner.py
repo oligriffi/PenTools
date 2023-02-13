1be39e5d6b237b1a1acf8ee0325829c569279b9e
oligriffi <106426514+oligriffi@users.noreply.github.com>
2023-02-13 22:26:34 +0000

    Update PortScanner.py

--- PortScanner.py
+++ PortScanner.py
@@ -1,4 +1,5 @@
 import socket
+import sys
 
 def is_port_open(host, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -7,18 +8,23 @@ def is_port_open(host, port):
     sock.close()
     return result == 0
 
-def scan_ports(host):
+def scan_ports(host, start_port, end_port):
     open_ports = []
-    for port in range(1, 65535):
+    for port in range(start_port, end_port + 1):
         if is_port_open(host, port):
             open_ports.append(port)
     return open_ports
 
-host = input("Enter the host to scan: ")
-open_ports = scan_ports(host)
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print("Usage: python PortScanner.py host start_port end_port")
+        sys.exit(1)
+    host = sys.argv[1]
+    start_port = int(sys.argv[2])
+    end_port = int(sys.argv[3])
+    open_ports = scan_ports(host, start_port, end_port)
 
-if open_ports:
-    print("Open ports:", open_ports)
-else:
-    print("No open ports found.")
-
+    if open_ports:
+        print("Open ports:", open_ports)
+    else:
+        print("No open ports found.")
